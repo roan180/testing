734ccbef525d33101e6cd08d6a33dc26da3ca5fe
Replit user <>
2023-05-09 04:11:23 +0000

    added multiply function

--- chrystal_method.py
+++ chrystal_method.py
@@ -1,6 +1,8 @@
 def add(a,b):
     return a + b
 
+def multiply(a,b):
+  return a * b
 #testing
 
 #testing again
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import chrystal_method
 print("hello world")
 
 print(chrystal_method.add(2, 2))
+
+print
