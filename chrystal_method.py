2a4cac9b6b1c438fb45a26eb1f37ed462cc46b04
roan180 <eric.borja.520@gmail.com>
2023-05-08 20:49:02 -0700

    I like to cook, and I like to bake

--- chrystal_method.py
+++ chrystal_method.py
@@ -1,4 +1,5 @@
 def add(a,b):
     return a + b
 
-#testing
+def subtract(a,b):
+    return a - b
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import chrystal_method
 print("hello world")
 
 print(chrystal_method.add(2, 2))
+
+print(chrystal_method.subtract(2,3))
